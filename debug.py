aca97c9c1fb40168366de7cfd75c86fdda074dbe
Anna <anna@vanharmelen.me>
2025-01-15 16:23:08 +0100

    test practice()

--- debug.py
+++ debug.py
@@ -4,3 +4,20 @@ of the 'action coupled null-cue' experiment.
 
 made by Anna van Harmelen, 2025
 """
+from trial import generate_stimuli_characteristics
+from set_up import get_settings
+from practice import practice
+
+testing = True
+
+monitor = {
+            "resolution": (1920, 1080),  # in pixels
+            "Hz": 60,  # screen refresh rate in Hz
+            "width": 33,  # in cm
+            "distance": 50,  # in cm
+        }
+
+directory = r"../../Testing/"
+settings = get_settings(monitor, directory, "red")
+
+practice(testing, "red", settings)
