d5d55fbdb65ccb468bb4d76cfaeb2b6e6a686565
Anna <58181940+annavanharmelen@users.noreply.github.com>
2025-07-15 13:54:41 +0200

    update name

--- eyetracker.py
+++ eyetracker.py
@@ -3,7 +3,7 @@ This file contains the functions necessary for
 connecting and using the eyetracker.
 To run the 'action coupled null-cue' experiment, see main.py.
 
-made by Anna van Harmelen, 2025, using code by Rose Nasrawi
+made by Anna van Harmelen, 2025, using code by Ezra Nasrawi
 """
 
 from lib import eyelinker
